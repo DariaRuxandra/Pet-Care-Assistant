b4e9f689d86831ce40a51e2664baa3b52dcdf87f
DariaRuxandra <dariaruxandra15@gmail.com>
2025-10-19 13:47:01 +0300

    Add project files.

---
+++ Pet Care Assistant/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Pet_Care_Assistant
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Pet Care Assistant/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Pet_Care_Assistant")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Pet_Care_Assistant.Pages")]
